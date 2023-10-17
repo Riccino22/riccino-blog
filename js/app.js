3d921adc067e608f2b85367bceed4b3951289e29
Riccardo <riccardorinojosam@gmail.com>
2023-10-17 18:13:27 -0400

    responsive 1

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let ic_menu = document.getElementById("icon-menu"); //we have create the var ic_menu for use it as reference
+
+//Now, we will declare a function called show_menu for show the vertical nav with an animation for mobile dispositives 
+
+function show_menu() {
+    document.getElementById("move-content").classList.toggle('move-container-all')
+}
+
+ic_menu.addEventListener("click", show_menu);
