65233c4cc9144d96ae7b35730393e3eede376cbe
Riccardo <riccardorinojosam@gmail.com>
2023-11-05 17:46:24 -0400

    articles created

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def art(request, id):
+    print(id)
+    return render(request, 'articles/' + str(id) + '.html')
+
+
 # Create your views here.
