905311f6b204d328d1deba422b2f1a0aba01bc2b
Riccardo <riccardorinojosam@gmail.com>
2023-11-05 11:39:57 -0400

    django added

--- blog_app/static/js/app.js
+++ blog_app/static/js/app.js
@@ -3,7 +3,8 @@ let ic_menu = document.getElementById("icon-menu"); //we have create the var ic_
 //Now, we will declare a function called show_menu for show the vertical nav with an animation for mobile dispositives 
 
 function show_menu() {
-    document.getElementById("move-content").classList.toggle('move-container-all')
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral')
 }
 
 ic_menu.addEventListener("click", show_menu);
