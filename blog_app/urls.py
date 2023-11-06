8ab2472d194f6fb197ae2a58db70bca46c1243e7
Riccardo <riccardorinojosam@gmail.com>
2023-11-06 14:23:04 -0400

    update urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('/home', views.home),
+    path('home/', views.home),
     path('<int:id>', views.art)
 ]
